fa3fd2dccfe992e7cd00e3ed7c89ce913c5ecfd2
arnavlimaye <arnav@technogise.com>
2021-10-23 01:49:35 +0530

    Setup lanchDarkly in basic create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import { useFlags } from 'launchdarkly-react-client-sdk';
 
-function App() {
+const App = () => {
+  const { SHOW_HOME_DIV } = useFlags();
+  console.log(useFlags());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {SHOW_HOME_DIV && <Home />}
+    </>
   );
-}
+};
 
 export default App;
